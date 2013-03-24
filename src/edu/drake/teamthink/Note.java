f160b32efa5f60c9a1bc254e770b17d0514e6fd6
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-03-24 13:53:21 -0500

    added Note Skeleton Class

---
+++ src/edu/drake/teamthink/Note.java
@@ -0,0 +1,31 @@
+package edu.drake.teamthink;
+import java.util.*;
+
+public class Note {
+	
+	private String text;
+	private String author;
+	private Date creationDate;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+
+
+}
