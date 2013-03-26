258a1c0f0f84e7460957a16b6d4c494dd4f1e69d
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-03-26 11:08:23 -0500

    renamed main activity to login

--- src/edu/drake/teamthink/LoginActivity.java
+++ src/edu/drake/teamthink/LoginActivity.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_login, menu);
 		return true;
 	}
 
