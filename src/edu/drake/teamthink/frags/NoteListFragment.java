19abe6f8f159e286706e2ce1038b6494c552556e
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-03-31 18:54:06 -0500

    Added buttons for refresh/add

--- src/edu/drake/teamthink/frags/NoteListFragment.java
+++ src/edu/drake/teamthink/frags/NoteListFragment.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import edu.drake.teamthink.Note;
+import edu.drake.teamthink.R;
 import edu.drake.teamthink.db.DBMethods;
 
 public class NoteListFragment extends ListFragment {
