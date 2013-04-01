23b3a6800eefcdd507ed7052e3e5e8d96211b351
Byron Varberg <btvarberg@gmail.com>
2013-03-31 19:46:51 -0500

    added spinner and spinner resources

--- src/edu/drake/teamthink/NoteActivity.java
+++ src/edu/drake/teamthink/NoteActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import edu.drake.teamthink.frags.*;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import edu.drake.teamthink.frags.NoteDetailFragment;
+import edu.drake.teamthink.frags.NoteListFragment;
 
 public class NoteActivity extends Activity implements NoteListFragment.OnNoteSelectedListener {
 	
@@ -13,6 +16,14 @@ public class NoteActivity extends Activity implements NoteListFragment.OnNoteSel
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_note);
+		Spinner spinner = (Spinner) findViewById(R.id.sort_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.sort_list, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 	}
 
 	@Override
